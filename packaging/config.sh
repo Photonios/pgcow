e9f2df75c897fd6568f1a450f53378252c42ea9f
Swen Kooij <swenkooij@gmail.com>
2019-09-08 21:26:32 +0300

    pgcow: change version number to 0.0.1~alpha1

--- packaging/config.sh
+++ packaging/config.sh
@@ -5,7 +5,7 @@ set -e
 cd "$(dirname $0)"
 
 package_name="pgcow"
-package_version="0.0.1"
+package_version="0.0.1~alpha1"
 package_dir="$(pwd)/${package_name}_${package_version}"
 install_dir="/opt/pgcow"
 
