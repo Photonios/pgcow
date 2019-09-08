f1e2f5488c53517080958217c82667678638426d
Swen Kooij <swenkooij@gmail.com>
2019-09-08 22:13:12 +0300

    pgcow: do not set LD_LIBRARY_PATH in profile.d script

--- packaging/pgcow_0.0.1~alpha1/etc/profile.d/pgcow.sh
+++ packaging/pgcow_0.0.1~alpha1/etc/profile.d/pgcow.sh
@@ -3,5 +3,3 @@
 set -e
 
 export PATH="/opt/pgcow/bin:$PATH"
-export LD_LIBRARY_PATH="/opt/pgcow/lib:/opt/pgcow/postgresql:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/opt/pgcow/lib/pkgconfig:$PKG_CONFIG_PATH"
